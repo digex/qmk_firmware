7233ae54d1e73046ab3fde065c5e75b3c3ac17ad
U-LEGION-1\Den <C:\Users\Den\AppData\Roaming\The Bat!>
2024-12-27 15:38:46 +0300

    2040

--- keyboards/handwired/digex/ortho_split_rgb_matrix_keyboard/ortho_split_rgb_matrix_keyboard.c
+++ keyboards/handwired/digex/ortho_split_rgb_matrix_keyboard/ortho_split_rgb_matrix_keyboard.c
@@ -150,11 +150,11 @@ bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
 
 
 void suspend_power_down_kb(void) {
-    //rgb_matrix_set_suspend_state(true);
+    rgb_matrix_set_suspend_state(true);
     //suspend_power_down_user();
 }
 
 void suspend_wakeup_init_kb(void) {
-    //rgb_matrix_set_suspend_state(false);
+    rgb_matrix_set_suspend_state(false);
     //suspend_wakeup_init_user();
 }
